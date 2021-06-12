5d1389b085229a98d6a693b4536b89231327145e
Devon Coates <63134707+dcoates83@users.noreply.github.com>
2021-06-11 18:24:38 -0600

    added notes to pages

--- src/pages/fetchData.js
+++ src/pages/fetchData.js
@@ -1,6 +1,44 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
+/*
+assign graphql to a variable
+- Must be a capital case
+ie:
+const data = graphql`
+    {
+      site {
+        siteMetadata {
+          author
+          complexData {
+            age
+            name
+          }
+          description
+          person {
+            age
+            name
+          }
+          simpleData
+          title
+        }
+      }
+    }
+  `
+----
+  static query needs to be invoked in the component tho 
+  SomeComponent =()=>{
+  somthing = useStaticQuery(data)
+  return(stuff)
+  }
+ ---
+ ###Field Alias
+myName:siteMetadata
+to access it is now {myName}
+
+
+*/
+
 const ComponentName = () => {
   const data = useStaticQuery(graphql`
     {
