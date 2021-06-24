394cb9523403a249ba1a0e7aa3c1647f78d2e0e0
Devon Coates <63134707+dcoates83@users.noreply.github.com>
2021-06-23 19:30:32 -0600

    updated Seo on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import SEO from "../components/SEO"
 export default function Home() {
   return (
     <Layout>
-      <SEO title="title" />
+      <SEO title="Home" />
       <main className="page">
         <header className="hero">
           <StaticImage
